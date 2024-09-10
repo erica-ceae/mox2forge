740646a684e18faeab028086083c23a80215457e
erica <159135353+erica-ceae@users.noreply.github.com>
2024-09-10 21:44:04 +0100

    handle foil promos, tidy up functions

--- mox2forge.py
+++ mox2forge.py
@@ -1,4 +1,5 @@
 import pyperclip
+import re
 
 # list of fragments forge seems to struggle with
 # any promo variant using the "P[set]" syntax, e.g. "PDOM" for "DOM" promos
@@ -11,18 +12,30 @@ def delist(card):
     card = card + ") " + str(num)
     return card
 
-list = pyperclip.paste().split("\n")
+def depromo(card):
+    if re.search(r"\(P[a-zA-Z0-9]{3}\)", card):
+        card = card.replace("(P", "(")
+    return card
 
-for i in range(0, len(list)):
-    card = list[i]
-    match card:
-        case card if "*F*" in card:
-            list[i] = card.replace("*F*", "")
-        case card if "(PLST)" in card:
-            list[i] = delist(card)
-        case card if "(P" in card:
-            list[i] = card.replace("(P", "(")
-        case _:
-            pass
+def main():
+    list = pyperclip.paste().split("\n")
+    
+    for i in range(0, len(list)):
+        card = list[i]
+        match card:
+            case card if "*F*" in card:
+                list[i] = card.replace("*F*", "")
+                # catch foil promos
+                list[i] = depromo(list[i])
+            case card if "(PLST)" in card:
+                list[i] = delist(card)
+            case card if "(P" in card:
+                print(card)
+                list[i] = depromo(card)
+            case _:
+                pass
+    
+    pyperclip.copy("\n".join(list))
 
-pyperclip.copy("\n".join(list))
+if __name__ == "__main__":
+    main()
